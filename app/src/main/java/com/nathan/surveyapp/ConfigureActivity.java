0a8b3865c290dce13167cf0cd8370d263c587460
nathangaut <nathan.gaut@target.com>
2019-11-02 20:32:54 -0500

    Comments

--- app/src/main/java/com/nathan/surveyapp/ConfigureActivity.java
+++ app/src/main/java/com/nathan/surveyapp/ConfigureActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 
 public class ConfigureActivity extends AppCompatActivity {
 
+    //Placed in a dialog box https://stackoverflow.com/questions/1979369/android-activity-as-a-dialog
+
     private static final int CONFIGURE_REQUEST_CODE = 3;
 
     public static final String EXTRA_QUESTION_QUESTION = "com.nathan.surveyapp.questionQuestion";
@@ -21,32 +23,33 @@ public class ConfigureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configure);
-
-        Button addQuestion = findViewById(R.id.add_question_button);
+        //set variables for views
+        final Button addQuestion = findViewById(R.id.add_question_button);
         final EditText questionText = findViewById(R.id.new_question);
         final EditText yesText = findViewById(R.id.yes_answer);
         final EditText noText = findViewById(R.id.no_answer);
+        //Onclick sets text of question and answers
         addQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String newQuestion = questionText.getText().toString() +"?";
                 String mYesText = yesText.getText().toString();
                 String mNoText = noText.getText().toString();
-
+                //If any fields are empty toasts
                 if (newQuestion.isEmpty() || mYesText.isEmpty() || mNoText.isEmpty()) {
                     Toast.makeText(ConfigureActivity.this, "Please enter some text", Toast.LENGTH_LONG).show();
                     return;
                 }
-
+                //Intent back to main with texts
                 Intent returnToMainIntent = new Intent(ConfigureActivity.this, MainActivity.class);
                 returnToMainIntent.putExtra(EXTRA_QUESTION_QUESTION, newQuestion);
                 returnToMainIntent.putExtra(EXTRA_QUESTION_ONE_TEXT, mYesText);
                 returnToMainIntent.putExtra(EXTRA_QUESTION_TWO_TEXT, mNoText);
-
+                //clears the fields
                 questionText.getText().clear();
                 yesText.getText().clear();
                 noText.getText().clear();
-
+                //Starts activity
                 startActivityForResult(returnToMainIntent, CONFIGURE_REQUEST_CODE);
             }
         });
